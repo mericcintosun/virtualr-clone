caea9d5a821cd66e32fb05ab5a71b0bfd5a1f383
meric <mericcintosunn@gmail.com>
2024-08-14 00:39:48 +0300

    final-project

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
+import Feature from './components/Feature'
+import Footer from './components/Footer'
+import Hero from './components/Hero'
 import { Navbar } from './components/Navbar'
+import Pricing from './components/Pricing'
+import Testiominals from './components/Testiominals'
+import Workflow from './components/Workflow'
 import './index.css'
 function App() {
 
@@ -6,6 +12,14 @@ function App() {
   return (
     <>
     <Navbar/>
+    <div className="max-w-7xl mx-auto pt-20 px-6">
+    <Hero/>
+    <Feature />
+    <Workflow />
+    <Pricing />
+    <Testiominals />
+    <Footer />
+    </div>
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,42 @@
+import { resourcesLinks , platformLinks , communityLinks} from "../constants"
+
+const Footer = () => {
+  return (
+    <footer className="border-t border-neutral-700 mt-20 py-10">
+        <div className="flex justify-between flex-wrap gap-8">
+            <div>
+            <h3 className="text-md font-semibold mb-4">Resources</h3>
+            <ul className="space-y-2">
+                {resourcesLinks.map((link, index) => (
+                    <li key={index}>
+                        <a className="text-neutral-300 hover:text-white" href={link.href}>{link.text}</a>
+                    </li>
+                ))}
+            </ul>
+            </div>
+            <div>
+            <h3 className="text-md font-semibold mb-4">Platform</h3>
+            <ul className="space-y-2">
+                {platformLinks.map((link, index) => (
+                    <li key={index}>
+                        <a className="text-neutral-300 hover:text-white" href={link.href}>{link.text}</a>
+                    </li>
+                ))}
+            </ul>
+            </div>
+            <div>
+            <h3 className="text-md font-semibold mb-4">Community</h3>
+            <ul className="space-y-2">
+                {communityLinks.map((link, index) => (
+                    <li key={index}>
+                        <a className="text-neutral-300 hover:text-white" href={link.href}>{link.text}</a>
+                    </li>
+                ))}
+            </ul>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
